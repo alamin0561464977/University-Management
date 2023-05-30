cb4b2e37e40dc887f38efa61d1edd1a0edeac5b3
alamin0561464977 <amin23435678901315@gmail.com>
2023-05-31 01:50:31 +0300

    [dune] setup mongoose, express, cors, mongodb, .env

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import { app } from "./index";
+import config from "./config/config";
+
+async function main() {
+    try {
+        await mongoose.connect(config.db);
+        console.log("Connected to MongoDB");
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+main();
+
+
+
+
+// name = University - Management
+// pzss = tkchVheBiCmbk4FI
